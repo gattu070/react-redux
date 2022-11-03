7539d29cbe6c8e658d94ffe6347cdb59abf66e38
GattuP07 <gautam.patoliya@toforzero.in>
2022-11-03 18:22:51 +0530

    added carts count on navbar cart icon

--- src/containers/HomeContainer.js
+++ src/containers/HomeContainer.js
@@ -1,3 +1,13 @@
+import { connect } from "react-redux";
+import { addToCart } from "../services/actions/action";
 import { Home } from "../components/Home";
 
-export default Home;
+const mapStateToProps = state => ({
+    // data: state.cardItems
+})
+
+const mapDispatchToProps = dispatch => ({
+    addToCartHandler: data => dispatch(addToCart(data))
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
--- src/services/reducers/reducer.js
+++ src/services/reducers/reducer.js
@@ -1,18 +1,18 @@
 import { ADD_TO_CART } from "../constants";
 import { REMOVE_TO_CART } from "../constants";
 
-
 const initialSate = {
     cardData: []
 }
 
-export default function cardItems(state = initialSate, action) {
+export default function cardItems(state = [], action) {
+    console.log("reducer", action);
     switch (action.type) {
         case ADD_TO_CART:
-            return {
+            return [
                 ...state,
-                cardData: action.data
-            }
+                { cardData: action.data }
+            ]
             break;
         default:
             return state;
