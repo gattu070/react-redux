e41a69f67a04c7c9841957219a853078c5b14bb7
GattuP07 <gautam.patoliya@toforzero.in>
2022-11-03 13:49:06 +0530

    action and reducer file integrated

---
+++ src/services/reducers/reducer.js
@@ -0,0 +1,20 @@
+import { ADD_TO_CART } from "../constants";
+import { REMOVE_TO_CART } from "../constants";
+
+
+const initialSate = {
+    cardData: []
+}
+
+export default function cardItems(state = initialSate, action) {
+    switch (action.type) {
+        case ADD_TO_CART:
+            return {
+                ...state,
+                cardData: action.data
+            }
+            break;
+        default:
+            return state;
+    }
+}
