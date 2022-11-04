0755fac7c76e1b384d449587cf579e0cb7dcfe40
GattuP07 <gautam.patoliya@toforzero.in>
2022-11-04 18:50:23 +0530

    error solved

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import "./css/style.css";
-import 'bootstrap/dist/css/bootstrap.min.css';
 import HomeContainer from './containers/HomeContainer';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "./css/style.css";
 import NavbarContainer from './containers/NavbarContainer';
 
 function App() {
